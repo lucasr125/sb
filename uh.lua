c6daca1bbeb270f4422829b4052a2d2e8569ad7b
dddd <140569929+lucasr125@users.noreply.github.com>
2023-09-19 14:59:48 -0300

    Update uh.lua

--- uh.lua
+++ uh.lua
@@ -1,6 +1,7 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "Title of the library", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 local Tab = Window:MakeTab({Name = "Tab 1",Icon = "rbxassetid://4483345998",PremiumOnly = false})
+local CustomizeGui = Window:MakeTab({Name = "GUI Customization ( WIP )",Icon = "rbxassetid://4483345998",PremiumOnly = false})
 --functions
 local Section = Tab:AddSection({Name = "Section"})
 OrionLib:MakeNotification({Name = "Title!",Content = "Notification content... what will it say??",Image = "rbxassetid://4483345998",Time = 5})
