572f2fd2d4eacf096b9fd2e02c2fdaaef5691338
dddd <140569929+lucasr125@users.noreply.github.com>
2023-10-01 10:49:53 -0300

    Update GodsHandAbilitySpam.lua

--- GodsHandAbilitySpam.lua
+++ GodsHandAbilitySpam.lua
@@ -9,7 +9,7 @@ wait(1)
 local teleportFunc = queueonteleport or queue_on_teleport or syn and syn.queue_on_teleport
 if teleportFunc then
     teleportFunc([[
-        
+        loadstring(game:HttpGet((https://raw.githubusercontent.com/lucasr125/sb/main/GodsHandAbilitySpam.lua)))()
     ]])
 end
 game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId, game.Players.LocalPlayer)
