f59d2f04075b801567636ab1b8a8e62c0393f23f
dddd <140569929+lucasr125@users.noreply.github.com>
2023-10-01 10:56:38 -0300

    Update GodsHandAbilitySpam.lua

--- GodsHandAbilitySpam.lua
+++ GodsHandAbilitySpam.lua
@@ -1,15 +1,13 @@
-fireclickdetector(workspace.Lobby["God's Hand"].ClickDetector)
 repeat
 	task.wait()
 until not game.Workspace:FindFirstChild("universaltimestop")
+fireclickdetector(workspace.Lobby["God's Hand"].ClickDetector)
 game:GetService("ReplicatedStorage").TimestopJump:FireServer()
 game:GetService("ReplicatedStorage").Timestopchoir:FireServer()
 game:GetService("ReplicatedStorage").Timestop:FireServer()
 wait(1)
 local teleportFunc = queueonteleport or queue_on_teleport or syn and syn.queue_on_teleport
 if teleportFunc then
-    teleportFunc([[
-        loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/sb/main/GodsHandAbilitySpam.lua")))()
-    ]])
+    teleportFunc([[loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/sb/main/GodsHandAbilitySpam.lua")))()]])
 end
 game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId, game.Players.LocalPlayer)
