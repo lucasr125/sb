d4944c1c43a1966692229234c3969a1e4ff9a802
dddd <140569929+lucasr125@users.noreply.github.com>
2023-10-01 11:02:04 -0300

    Update GodsHandAbilitySpam.lua

--- GodsHandAbilitySpam.lua
+++ GodsHandAbilitySpam.lua
@@ -6,8 +6,5 @@ game:GetService("ReplicatedStorage").TimestopJump:FireServer()
 game:GetService("ReplicatedStorage").Timestopchoir:FireServer()
 game:GetService("ReplicatedStorage").Timestop:FireServer()
 wait(1)
-local teleportFunc = queueonteleport or queue_on_teleport or syn and syn.queue_on_teleport
-
-teleportFunc([[loadstring(game:HttpGet("https://raw.githubusercontent.com/lucasr125/sb/main/GodsHandAbilitySpam.lua"))()]])
 
 game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId, game.Players.LocalPlayer)
