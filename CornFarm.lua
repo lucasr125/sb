74b5e7bc8a0439fa679bc4bb856b85f19107b6d0
dddd <140569929+lucasr125@users.noreply.github.com>
2023-10-14 23:33:54 -0300

    Update CornFarm.lua

--- CornFarm.lua
+++ CornFarm.lua
@@ -1,28 +1,36 @@
 if not game:IsLoaded() then
-  game.Loaded:Wait()
+	game.Loaded:Wait();
 end
 
-repeat task.wait() until game:GetService("Players")
-repeat task.wait() until game.Players.LocalPlayer
-repeat task.wait() until game.Players.LocalPlayer.Character ~= nil
-repeat task.wait() until game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
+repeat
+	task.wait();
+until game:GetService("Players") 
+repeat
+	task.wait();
+until game.Players.LocalPlayer 
+repeat
+	task.wait();
+until game.Players.LocalPlayer.Character ~= nil 
+repeat
+	task.wait();
+until game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
 
 for _, v in pairs(game.Workspace.CandyCorns:GetDescendants()) do
-	if v.ClassName == "TouchTransmitter" then
-		v.Parent.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-		firetouchinterest(v.Parent, game.Players.LocalPlayer.Character.HumanoidRootPart, 1)
-		firetouchinterest(v.Parent, game.Players.LocalPlayer.Character.HumanoidRootPart, 0)
+	if (v.ClassName == "TouchTransmitter") then
+		v.Parent.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame;
+		firetouchinterest(v.Parent, game.Players.LocalPlayer.Character.HumanoidRootPart, 1);
+		firetouchinterest(v.Parent, game.Players.LocalPlayer.Character.HumanoidRootPart, 0);
 	end
 end
 
-local serverList = {}
+local serverList = {};
 for _, v in ipairs(game:GetService("HttpService"):JSONDecode(game:HttpGetAsync("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100")).data) do
-	if v.playing and type(v) == "table" and v.maxPlayers > v.playing and v.id ~= game.JobId then
-		serverList[#serverList + 1] = v.id
+	if (v.playing and (type(v) == "table") and (v.maxPlayers > v.playing) and (v.id ~= game.JobId)) then
+		serverList[#serverList + 1] = v.id;
 	end
 end
-if #serverList > 0 then
-	game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, serverList[math.random(1, #serverList)])
+if (#serverList > 0) then
+	game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, serverList[math.random(1, #serverList)]);
 else
-    error("No servers found")
+	error("No servers found");
 end
