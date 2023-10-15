2cf01dde6fd1f94f2bb5712faf42376112fe6664
dddd <140569929+lucasr125@users.noreply.github.com>
2023-10-14 23:23:25 -0300

    Update GodsHandAbilitySpam.lua
    Fixed ver.

--- GodsHandAbilitySpam.lua
+++ GodsHandAbilitySpam.lua
@@ -1,5 +1,14 @@
+if not game:IsLoaded() then
+  game.Loaded:Wait()
+end
+
+repeat task.wait() until game:GetService("Players")
+repeat task.wait() until game.Players.LocalPlayer
+repeat task.wait() until game.Players.LocalPlayer.Character ~= nil
+repeat task.wait() until game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
+
 repeat task.wait()
-until not game.Workspace:FindFirstChild("universaltimestop")
+until game.Workspace:FindFirstChild("universaltimestop") == nil
 
 fireclickdetector(workspace.Lobby["God's Hand"].ClickDetector)
 game:GetService("ReplicatedStorage").TimestopJump:FireServer()
