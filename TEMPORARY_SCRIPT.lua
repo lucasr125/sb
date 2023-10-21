b769331e75d07aa344e9583c14e17e9223955411
dddd <140569929+lucasr125@users.noreply.github.com>
2023-10-20 22:14:04 -0300

    Create TEMPORARY_SCRIPT.lua

---
+++ TEMPORARY_SCRIPT.lua
@@ -0,0 +1,261 @@
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Mushroom")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Mushroom")
+-- Make Potion
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("BrewPotion")
+-- grug potion
+
+
+
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Dark Root")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Dark Root")
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Dark Root")
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Dark Root")
+-- Make Potion
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("BrewPotion")
+-- nightmare potion
+
+
+
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Red Crystal")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Red Crystal")
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Blue Crystal")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Blue Crystal")
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Glowing Mushroom")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Glowing Mushroom")
+-- Make Potion
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("BrewPotion")
+-- confusion potion
+
+
+
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Dire Flower")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Dire Flower")
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Red Crystal")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Red Crystal")
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Wild Vine")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Wild Vine")
+-- Make Potion
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("BrewPotion")
+-- power potion
+
+
+
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Plane Flower")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Plane Flower")
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Plane Flower")
+-- Make Potion
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("BrewPotion")
+-- paralyzing potion
+
+
+
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Autumn Sprout")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Autumn Sprout")
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Jade Stone")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Jade Stone")
+-- Make Potion
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("BrewPotion")
+-- haste potion
+
+
+
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Hazel Lily")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Hazel Lily")
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Hazel Lily")
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Blue Crystal")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Blue Crystal")
+-- Make Potion
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("BrewPotion")
+-- invisibility potion
+
+
+
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Red Crystal")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Red Crystal")
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Fire Flower")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Fire Flower")
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Fire Flower")
+-- Make Potion
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("BrewPotion")
+-- expotion potion
+
+
+
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Elder Wood")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Elder Wood")
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Mushroom")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Mushroom")
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Mushroom")
+-- Make Potion
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("BrewPotion")
+-- invencible potion
+
+
+
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Dark Root")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Dark Root")
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Dark Root")
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Blood Rose")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Blood Rose")
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Red Crystal")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Red Crystal")
+-- Make Potion
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("BrewPotion")
+-- toxic potion
+
+
+
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Winter Rose")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Winter Rose")
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Winter Rose")
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Wild Vine")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Wild Vine")
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Blue Crystal")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Blue Crystal")
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Glowing Mushroom")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Glowing Mushroom")
+-- Make Potion
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("BrewPotion")
+-- freeze potion
+
+
+
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Mushroom")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Mushroom")
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Hazel Lily")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Hazel Lily")
+-- Make Potion
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("BrewPotion")
+-- feather potion ( not working for now )
+
+
+
+
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Mushroom")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Mushroom")
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Mushroom")
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Plane Flower")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Plane Flower")
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Hazel Lily")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Hazel Lily")
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Blue Crystal")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Blue Crystal")
+-- Make Potion
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("BrewPotion")
+-- speed potion
+
+
+
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Blood Rose")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Blood Rose")
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Blood Rose")
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Blood Rose")
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Blood Rose")
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Blood Rose")
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Blood Rose")
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Blood Rose")
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Blood Rose")
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Blood Rose")
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Blood Rose")
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Dark Root")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Dark Root")
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Dark Root")
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Dark Root")
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Dark Root")
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Dark Root")
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Dark Root")
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Dark Root")
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Dark Root")
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Dark Root")
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Dark Root")
+-- Make Potion
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("BrewPotion")
+-- lethal potion
+
+
+
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Mushroom")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Mushroom")
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Mushroom")
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Blue Crystal")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Blue Crystal")
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Blue Crystal")
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Jade Stone")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Jade Stone")
+-- Equip item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("EquipItem", "Plane Flower")
+-- Add item
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("MixItem", "Plane Flower")
+-- Make Potion
+game:GetService("ReplicatedStorage").AlchemistEvent:FireServer("BrewPotion")
+-- slow potion
