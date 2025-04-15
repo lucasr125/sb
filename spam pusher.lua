35255490b3fef4814b5bde4f8afed0361f19d317
dddd <140569929+lucasr125@users.noreply.github.com>
2025-04-15 02:01:21 -0300

    Create spam pusher.lua

---
+++ spam pusher.lua
@@ -0,0 +1,17 @@
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
+local RootPart = Character:WaitForChild("HumanoidRootPart")
+firetouchinterest(game.Players.LocalPlayer.Character:WaitForChild("Head"), workspace.Lobby.Teleport1.TouchInterest.Parent, 0)
+firetouchinterest(game.Players.LocalPlayer.Character:WaitForChild("Head"), workspace.Lobby.Teleport1.TouchInterest.Parent, 1)
+for i = 1, 5 do
+    for _, player in ipairs(Players:GetPlayers()) do
+	if player ~= LocalPlayer and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+		local targetCFrame = player.Character.HumanoidRootPart.CFrame
+        RootPart.CFrame = targetCFrame + Vector3.new(-2, -2, -2)
+           game:GetService("ReplicatedStorage"):WaitForChild("PusherWall"):FireServer()
+           task.wait()
+    end
+    end
+end
+game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId, Players.LocalPlayer)
